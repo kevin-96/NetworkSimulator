544a91b49565f3e61b22e672036c412375cc8432
Phillip Nam <pnam@Phillips-MBP.home>
2021-04-02 18:19:39 -0400

    dijkstra in progress

--- LinkStateRouter.java
+++ LinkStateRouter.java
@@ -31,15 +31,6 @@ public class LinkStateRouter extends Router {
 
     }
 
-    /* TODO: Construct the Link State Packet
-    Packet includes:
-    - Identity of sender
-    - Sequence number (32-bit) and age (decrement age after each second)
-    - List of neighbors
-    */
-
-    // Determining when the packet is constructed (Important)
-
     public static class PingPacket extends Packet {
         long pingTime;
 
@@ -61,7 +52,7 @@ public class LinkStateRouter extends Router {
     }
     
     public static class LinkStatePacket extends Packet {
-        Map<Integer,Long> costs; // Link state packet contain all of the information it has learned from its neighbors
+        Map<Integer,Long> costs; // Link state packet contains all of the information it has learned from its neighbors
 
         public LinkStatePacket(int source, int dest, int hopCount, Map<Integer,Long> costs) {
             // The constructor automatically sets the payload to be the delta time
@@ -77,7 +68,7 @@ public class LinkStateRouter extends Router {
     public LinkStateRouter(int nsap, NetworkInterface nic) {
         super(nsap, nic);
         debug = Debug.getInstance();  // For debugging!
-        neighborCosts = new HashMap<>(); // 
+        neighborCosts = new HashMap<>(); // Each router knows the costs of its neighbors
     }
 
     private void findCosts() {
@@ -86,18 +77,94 @@ public class LinkStateRouter extends Router {
             int neighbor = neighbors.get(i);
             Packet pingPacket = new PingPacket(this.nsap, neighbor, 1);
             LinkStatePacket linkStatePacket = new LinkStatePacket(this.nsap, neighbor, 1, this.costs) // Link State Packet created
-            nic.sendOnLink(i, pingPacket); // could also use sendOnLink (or toSend?)
+            nic.sendOnLink(i, pingPacket); // Send out the ping packet
             nic.sendOnLink(i, linkStatePacket); // Send out link state packet
         }
         // TODO: Build the routing table based on the graph (Hashmap of costs, hashmap of hashmap)
         Map<Integer, Map<Integer, Long>> routingTable
         
-
         // Debug: Print a node's graph
-        // Perform Djikstra's Algorithm for the routing table (Make function call)
+        System.out.println(/* a node */);
+
+        // TODO: Perform Djikstra's Algorithm for the routing table (Make function call)
+        shortestPath(routingTable);
     }
 
 /////////////////////////////////////////////////////////
+    // TODO: Find the shortest path for each router using Dijkstra's Algorithm (GfG)
+
+    class ShortestPath {
+    // A utility function to find the vertex with minimum distance value,
+    // from the set of vertices not yet included in shortest path tree
+    static final int V = 9;
+    int minDistance(int dist[], Boolean sptSet[])
+    {
+        // Initialize min value
+        int min = Integer.MAX_VALUE, min_index = -1;
+  
+        for (int v = 0; v < V; v++)
+            if (sptSet[v] == false && dist[v] <= min) {
+                min = dist[v];
+                min_index = v;
+            }
+  
+        return min_index;
+    }
+  
+    // A utility function to print the constructed distance array
+    void printSolution(int dist[], int n)
+    {
+        System.out.println("Vertex   Distance from Source");
+        for (int i = 0; i < V; i++)
+            System.out.println(i + " tt " + dist[i]);
+    }
+  
+    // Function that implements Dijkstra's single source shortest path
+    // algorithm for a graph represented using adjacency matrix
+    // representation
+    void dijkstra(int graph[][], int src)
+    {
+        int dist[] = new int[V]; // The output array. dist[i] will hold
+        // the shortest distance from src to i
+  
+        // sptSet[i] will true if vertex i is included in shortest
+        // path tree or shortest distance from src to i is finalized
+        Boolean sptSet[] = new Boolean[V];
+  
+        // Initialize all distances as INFINITE and stpSet[] as false
+        for (int i = 0; i < V; i++) {
+            dist[i] = Integer.MAX_VALUE;
+            sptSet[i] = false;
+        }
+  
+        // Distance of source vertex from itself is always 0
+        dist[src] = 0;
+  
+        // Find shortest path for all vertices
+        for (int count = 0; count < V - 1; count++) {
+            // Pick the minimum distance vertex from the set of vertices
+            // not yet processed. u is always equal to src in first
+            // iteration.
+            int u = minDistance(dist, sptSet);
+  
+            // Mark the picked vertex as processed
+            sptSet[u] = true;
+  
+            // Update dist value of the adjacent vertices of the
+            // picked vertex.
+            for (int v = 0; v < V; v++)
+  
+                // Update dist[v] only if is not in sptSet, there is an
+                // edge from u to v, and total weight of path from src to
+                // v through u is smaller than current value of dist[v]
+                if (!sptSet[v] && graph[u][v] != 0 && 
+                   dist[u] != Integer.MAX_VALUE && dist[u] + graph[u][v] < dist[v])
+                    dist[v] = dist[u] + graph[u][v];
+        }
+  
+        // print the constructed distance array
+        printSolution(dist, V);
+    }
 
 
 ////////////////////////////////////////////////////////
